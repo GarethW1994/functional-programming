148bb5549e3e6a584186772e99774bec75986a70
Gareth <williams.gareth0@gmail.com>
2018-03-09 20:37:37 +0200

    added method functions

--- js/map.js
+++ js/map.js
@@ -1,13 +1,13 @@
 var someArray = [4, 9, 16],
-    sum = 0,
+    mysum = 0,
     avg = 0,
     highest = 0,
     sqrtArray = [];
 
 // map function
 sqrtArray = someArray.map(Math.sqrt);
-someArray.map((num) => { sum = sum + num });
-avg = Math.round(sum / someArray.length);
+someArray.map((num) => { mysum = mysum + num });
+avg = Math.round(mysum / someArray.length);
 
 someArray.map(getMax);
 
@@ -26,7 +26,7 @@ function appendOutput(input, process, output) {
 }
 
 appendOutput("Sqrt", "Math.sqrt()", sqrtArray);
-appendOutput("Sum", "sum += num", sum);
+appendOutput("Sum", "sum += num", mysum);
 appendOutput("Avg", "sum / array.length", avg);
 appendOutput("Max", "Math.max()", highest);
 
@@ -34,12 +34,9 @@ appendOutput("Max", "Math.max()", highest);
 $('#process').on('click', () => {
     // get input
     const numbers = $('#csnumbers').val();
-    $('input[name="operations"]:checked').each(function() {
-        this.value
-    });
+    const operations = $('input[name="operations"]:checked');
 
-    console.log(operations)
-        // check if the input is valid
+    // check if the input is valid
     const valid = onInputCheck(numbers) ? true : alert("Something Went Horribly Wrong!");
 
     // if the input was valid split the numbers
@@ -48,9 +45,21 @@ $('#process').on('click', () => {
     // if the numbers has been split then convert each number to integer
     const newNumericList = splitList.length > 0 ? splitList.map(convertNumeric) : false;
 
-    // determine which check box was clicked
+    // map the selected operations
+    const currentOperations = onGetOperations(operations);
 
+    // check if a operation was selected
+    const selectedOperations = currentOperations["length"] > 0 ? processOperation(currentOperations, newNumericList) : alert('Please Select A Operation');
 });
+
+const onGetOperations = (checkboxes) => {
+    const getValue = (index, operation) => operation["value"];
+    const getOperationValue = operations =>
+        operations.map(getValue)
+    const operationValues = getOperationValue(checkboxes);
+
+    return operationValues;
+}
 
 const onInputCheck = (listOfNumbers) => {
     const validate = numbers =>
@@ -65,3 +74,29 @@ const splitNumbers = (listOfNumbers) => {
 };
 
 const convertNumeric = (eachNumber) => Number(eachNumber);
+
+const processOperation = (listOfOperations, listOfNumbers) => {
+    const callRespectiveFunc = (index, value) => eval(value);
+
+    const checkFunc = operation =>
+        operation.map(callRespectiveFunc);
+
+    let newFunc = checkFunc(listOfOperations);
+
+    callRespectiveFunctions(newFunc, listOfNumbers)
+}
+
+const callRespectiveFunctions = (methods, numbers) => {
+    let value = methods[0];
+
+    console.log(value(numbers))
+}
+
+const squareroot = (numberList) => {
+    console.log(numberList)
+    console.log(Math.max(numberList))
+}
+
+const sum = () => {
+    console.log('sum method running')
+}
